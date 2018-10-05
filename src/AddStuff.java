0cc75b11f8aa3900642e7e83d6406faf6dba5776
Nicole Anderson-Au <nicoleanderson-au@Nicoles-MacBook-Pro.local>
2018-10-05 14:32:54 -0700

    Add necessary files

---
+++ src/AddStuff.java
@@ -0,0 +1,13 @@
+
+public class AddStuff {
+
+	public static void main(String[] args) {
+		System.out.println(AddStuff.sum(1, 2));
+
+	}
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
+}
